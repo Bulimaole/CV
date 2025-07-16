e44ee0693cf45101e1a15a8746387b3e0456731e
Bulimaole <15209806041@163.com>
2025-07-15 21:38:58 -0700

    Add files via upload

---
+++ week16/pytorch-openpose-master/pytorch-openpose-master/src/hand_model_outputszie.py
@@ -0,0 +1,21 @@
+"""
+chenxiaohu
+
+"""
+import torch
+from tqdm import tqdm
+import json
+
+from src.model import handpose_model
+
+model = handpose_model()
+
+size = {}
+for i in tqdm(range(10, 1000)):
+    data = torch.randn(1, 3, i, i)
+    if torch.cuda.is_available():
+        data = data.cuda()
+    size[i] = model(data).size(2)
+
+with open('hand_model_output_size.json', 'w') as f:
+    json.dump(size, f)
