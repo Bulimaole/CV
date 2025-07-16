31ed36ef920ec43f218e06f85c0d7bfc34309438
Bulimaole <15209806041@163.com>
2025-07-15 21:35:28 -0700

    Add files via upload

---
+++ week10/pytorch/minsitent bc.py
@@ -0,0 +1,114 @@
+"""
+
+陈小虎
+minsitent bc
+
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+class Models:
+    def __init__(self, net, cost, optimist):
+        self.net = net
+        self.cost = self.create_cost(cost)
+        self.optimizer = self.create_optimizer(optimist)
+        pass
+
+    def create_cost(self, cost):
+        support_cost = {
+            'CROSS_ENTROPY': nn.CrossEntropyLoss(),
+            'MSE':nn.MSELoss()
+        }
+
+        return support_cost[cost]
+
+    def create_optimizer(self, optimist, **rests):
+        support_optim = {
+            'SGD': optim.SGD(self.net.parameters(), lr=0.01, **rests),
+            'ADAM': optim.Adam(self.net.parameters(), lr=0.001, **rests),
+            'RMSP':optim.RMSprop(self.net.parameters(), lr=0.001, **rests)
+        }
+
+        return support_optim[optimist]
+
+    def train(self, train_loader, epochs=3, device='cpu'):
+        self.net.train()
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.net(inputs)
+                loss = self.cost(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item()
+                if i % 100 == 0:
+                    print('[epoch %d, %.2f%%] loss: %.3f' %
+                          (epoch + 1, (i + 1)*1./len(train_loader), running_loss / 100))
+                    running_loss = 0.0
+
+        print('Finished Training')
+
+    def evaluate(self, test_loader, device):
+        self.net.to(device)
+        print('Evaluating ...')
+        correct = 0
+        total = 0
+        with torch.no_grad(): # no grad when test and predict
+            for data in test_loader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+
+                outputs = self.net(images)
+                predicted = torch.argmax(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+
+def mnist_load_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize([0.5], [0.5])])
+
+    trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                          download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
+                                                    shuffle=True, num_workers=2)
+    testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                         download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=32,shuffle=True, num_workers=2)
+    return trainloader, testloader
+
+class MnistNet(torch.nn.Module):
+    def __init__(self):
+        super(MnistNet, self).__init__()
+        self.fc1 = torch.nn.Linear(28*28, 512)
+        self.fc2 = torch.nn.Linear(512, 512)
+        self.fc3 = torch.nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__=='__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # train for mnist
+    net = MnistNet().to(device)
+    model = Models(net, 'CROSS_ENTROPY', 'RMSP')
+    train_loader, test_loader = mnist_load_data()
+    model.train(train_loader, device=device)
+    model.evaluate(test_loader, device=device)
